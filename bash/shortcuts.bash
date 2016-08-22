3ab3909c3cf9a3730fb0fc71d59910e6be2b8b69
clamsey <clsmi21@gmail.com>
2016-08-22 11:46:20 -0400

    Create shortcuts.bash

---
+++ bash/shortcuts.bash
@@ -0,0 +1,20 @@
+# helpful commmand line shortcut aliases
+
+# get out of directories quickly
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# do openssl hashing easily
+alias sha1="openssl sha1"
+alias md5="openssl md5"
+
+# don't type sudo <package manager> every time
+alias yum="sudo yum"
+
+# ls shortcuts
+alias ll="ls -l"
+
+# jupyter notebook shortcut
+alias jnb="jupyter notebook"
